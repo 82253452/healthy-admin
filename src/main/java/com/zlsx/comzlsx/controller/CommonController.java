6dd2279afec76ed07d81a69d9715d81277e1cfd0
yuping <yuping@zhonglanmedia.com>
2019-09-22 14:48:42 +0800

    init

--- src/main/java/com/zlsx/comzlsx/controller/CommonController.java
+++ src/main/java/com/zlsx/comzlsx/controller/CommonController.java
@@ -1,15 +1,22 @@
 package com.zlsx.comzlsx.controller;
 
 import com.qiniu.util.Auth;
+import com.zlsx.comzlsx.config.WxOpenService;
 import com.zlsx.comzlsx.util.common.ForeseenException;
 import com.zlsx.comzlsx.util.common.Result;
+import me.chanjar.weixin.common.bean.WxJsapiSignature;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/common")
 public class CommonController {
+    @Resource
+    private WxOpenService wxOpenService;
 
     private final static String accessKey = "QGwnW_OYnSO3QR2osJ2mwqManZECHvFZ1tPq8bAv";
     private final static String secretKey = "xQzpa36UhxHWj6PXHgNJJeGiFQifbOxVLDZ5zWue";
@@ -23,5 +30,10 @@ public class CommonController {
         Auth auth = Auth.create(xiehe_accessKey, xiehe_secretKey);
         String upToken = auth.uploadToken(xiehe_bucket);
         return Result.ok(upToken);
+    }
+    @GetMapping("/jsapiSignature")
+    public Result weixinJSDk(String appId,String url) throws ForeseenException, WxErrorException {
+        WxJsapiSignature jsapiSignature = wxOpenService.getWxOpenComponentService().getWxMpServiceByAppid(appId).createJsapiSignature(url);
+        return Result.ok(jsapiSignature);
     }
 }
